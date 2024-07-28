22e2df939e27b4048bea84b9f68c59f9ebe35252
Kazik-Tazik <kazimir.yurkevich@gmail.com>
2024-07-28 13:53:26 +0200

    Kamizelka now attacking pigs

--- src/main/java/net/yurkevichkazimir/tryingmod/entity/custom/KamizelkaEntity.java
+++ src/main/java/net/yurkevichkazimir/tryingmod/entity/custom/KamizelkaEntity.java
@@ -1,5 +1,9 @@
 package net.yurkevichkazimir.tryingmod.entity.custom;
 
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.entity.monster.AbstractSkeleton;
+import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 import net.yurkevichkazimir.tryingmod.entity.ModEntities;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
@@ -61,15 +65,15 @@ public class KamizelkaEntity extends Animal {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.15D));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.2D, Ingredient.of(ModItem.CHUGUNOK.get()), false));
-
         this.goalSelector.addGoal(3, new FollowParentGoal(this, 1.1D));
-
         this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 1.1D));
-        this.goalSelector.addGoal(5, new LookAtPlayerGoal(this, Player.class, 5f));
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0, true));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 5f));
+        this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal(this, Pig.class, false));
 
     }
 
@@ -78,7 +82,10 @@ public class KamizelkaEntity extends Animal {
                 .add(Attributes.MAX_HEALTH, 10D)
                 .add(Attributes.FOLLOW_RANGE, 24D)
                 .add(Attributes.MOVEMENT_SPEED, 0.3D)
-                .add(Attributes.ARMOR_TOUGHNESS, 0.01f);
+                .add(Attributes.ARMOR_TOUGHNESS, 0.01f)
+                .add(Attributes.ATTACK_DAMAGE, 1.0)
+                .add(Attributes.ATTACK_KNOCKBACK, 1.0)
+                .add(Attributes.ATTACK_SPEED, 2.0);
     }
 
     @Nullable

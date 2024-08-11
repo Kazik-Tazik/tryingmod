2dd938b0316032492f656acb3ac8a8d5e2e440dd
Kazik-Tazik <kazimir.yurkevich@gmail.com>
2024-08-11 17:11:42 +0200

    Potato now drops

--- src/main/java/net/yurkevichkazimir/tryingmod/entity/custom/PotatoProjectileEntity.java
+++ src/main/java/net/yurkevichkazimir/tryingmod/entity/custom/PotatoProjectileEntity.java
@@ -9,7 +9,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.yurkevichkazimir.tryingmod.entity.ModEntities;
+import net.yurkevichkazimir.tryingmod.item.ModItem;
 
 public class PotatoProjectileEntity extends ThrowableItemProjectile {
     public PotatoProjectileEntity(EntityType<? extends ThrowableItemProjectile> pEntityType, Level pLevel) {
@@ -27,17 +29,27 @@ public class PotatoProjectileEntity extends ThrowableItemProjectile {
 
     @Override
     protected Item getDefaultItem() {
-        return Items.POTATO;
+        return ModItem.POTATO_PROJECTILE.get();
     }
 
-    @Override
     protected void onHitEntity(EntityHitResult pResult) {
         super.onHitEntity(pResult);
         Entity hitEntity = pResult.getEntity();
 
         hitEntity.hurt(this.damageSources().thrown(this, this.getOwner()), 5.0F);
 
+        if (!this.level().isClientSide() && this.level().random.nextFloat() < 0.8F) {
+            // Convert this entity to an item stack and drop it
+            this.spawnAtLocation(this.getItem());
+        }
         this.discard();
-        super.onHitEntity(pResult);
+    }
+
+    protected void onHit(HitResult pResult) {
+        super.onHit(pResult);
+        if (!this.level().isClientSide() && this.level().random.nextFloat() < 0.8F) {
+            this.spawnAtLocation(this.getItem());
+        }
+        this.discard();
     }
 }
